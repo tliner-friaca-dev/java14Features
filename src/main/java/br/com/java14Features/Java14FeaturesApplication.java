6ce5eddcf1bbaa1bab542214c2a87fef4417075b
tliner-friaca-dev <tlinerfc@hotmail.com>
2023-06-13 10:09:08 -0300

    first commit

---
+++ src/main/java/br/com/java14Features/Java14FeaturesApplication.java
@@ -0,0 +1,19 @@
+package br.com.java14Features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java14FeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java14FeaturesApplication.class, args);
+
+		System.out.println("\n----------------------");
+		System.out.println("ENTROU NA APLICAÇÃO!!!");
+		System.out.println("----------------------\n");
+		
+
+	}
+
+}
---
+++ src/test/java/br/com/java14Features/Java14FeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java14Features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java14FeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
